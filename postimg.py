869e22bbe239f7ad2a48553d99ccc08a355cb9d5
maplebeats <maplebeats@gmail.com>
2012-04-03 02:06:19 +0800

    增加对宽度的判断‘

--- postimg.py
+++ postimg.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 import fileinput,re
-img = re.compile(r'<a href=(.*)(<img src(.*)width=).*></a>')
+img = re.compile(r'<a href=.*(<img src(.*)width=)\"(.{3})\" height=\"(.{3})\".*></a>')
 for line in fileinput.input(inplace=1):
         line = line.rstrip()
-        image = re.findall
-        if re.search(img,line):
-                print(re.sub(img,r'\2"650">',line))
+        test = re.search(img,line)
+        if test:
+                if test.group(3) > test.group(4):
+                        print(re.sub(img,r'\1"400">',line))
+                else:
+                        print(re.sub(img,r'\1"650">',line))
         else:
                 print(line)
