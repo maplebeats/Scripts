f68a4b048587ff1e213368af1ea61f1bca5246da
maplebeats <maplebeats@gmail.com>
2012-05-18 00:19:58 +0800

    fix spell mistake

--- optirun.sh
+++ optirun.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#this script is used to resolve the nvidia random start
+#this script is used to resolve the nvidia card power on atrandom 
 #Tips: I can't ensure it's correctness
 
 optirun $1
