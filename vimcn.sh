a7784389f117b40759c77c9e3ea8938d58186a44
maplebeats <maplebeats@gmail.com>
2012-05-30 21:47:13 +0800

    add a script

---
+++ vimcn.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#贴代码，贴图片！
+
+ename=${1##*.}
+if [ "$ename" = "png" ] || [ "$ename" = "jpg" ] ;then
+    curl -F "name=@$1" http://img.vim-cn.com/ | xsel
+else
+    $* | curl -F 'vimcn=<-' http://p.vim-cn.com | xsel
+fi
