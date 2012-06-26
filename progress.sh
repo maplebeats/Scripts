846f5d422e3e6a92c9923d1af2217488ef15aa56
枫叶饭团 <maplebeats@gmail.com>
2012-06-26 16:17:08 +0800

    fix a mistake

--- progress.sh
+++ progress.sh
@@ -2,7 +2,7 @@
 time1=`date +'%s'`
 go() {
     time2=`date +'%s'`
-    for ((i=0;j<=$(($time2-$time1));j+=1))
+    for ((;i<=$(($time2-$time1));i+=1))
         do
             printf ">"
         done
