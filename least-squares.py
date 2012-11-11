c1afb93d12ef80a59faaa98bd1d2ef4676d6a5e0
枫叶饭团 <maplebeats@gmail.com>
2012-11-11 19:54:27 +0800

    least squares

---
+++ least-squares.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+mv=x0+x1*g
+https://zh.wikipedia.org/zh/最小二乘法
+
+'''
+
+mv = [int(i) for i in input("").split()]
+g = [int(i) for i in input("").split()]
+
+print((lambda mv,g:'mv='+str((lambda mv,g:sum(mv)/len(mv)-((lambda mv,g:sum([(i-sum(mv)/len(mv))*(j-sum(g)/len(g)) for i,j in zip(mv,g)])/sum([(i-sum(g)/len(g))**2 for i in g]))(mv,g))*(sum(mv)/len(mv)))(mv,g))+'+'+str(sum([(i-sum(mv)/len(mv))*(j-sum(g)/len(g)) for i,j in zip(mv,g)])/sum([(i-sum(g)/len(g))**2 for i in g]))+'*g')(mv,g))
