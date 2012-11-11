58cfb26da166458d834e0265ec24229a90f574db
枫叶饭团 <maplebeats@gmail.com>
2012-11-11 23:27:06 +0800

    update

--- least-squares.py
+++ least-squares.py
@@ -5,8 +5,4 @@ mv=x0+x1*g
 https://zh.wikipedia.org/zh/最小二乘法
 
 '''
-
-mv = [int(i) for i in input("Please input mv:").split()]
-g = [int(i) for i in input("Please input g").split()]
-
-print((lambda mv,g:'mv='+str((lambda mv,g:sum(mv)/len(mv)-((lambda mv,g:sum([(i-sum(mv)/len(mv))*(j-sum(g)/len(g)) for i,j in zip(mv,g)])/sum([(i-sum(g)/len(g))**2 for i in g]))(mv,g))*(sum(mv)/len(mv)))(mv,g))+'+'+str(sum([(i-sum(mv)/len(mv))*(j-sum(g)/len(g)) for i,j in zip(mv,g)])/sum([(i-sum(g)/len(g))**2 for i in g]))+'*g')(mv,g))
+print((lambda mv,g:'mv='+str((lambda mv,g:sum(mv)/len(mv)-((lambda mv,g:sum([(i-sum(mv)/len(mv))*(j-sum(g)/len(g)) for i,j in zip(mv,g)])/sum([(i-sum(g)/len(g))**2 for i in g]))(mv,g))*(sum(mv)/len(mv)))(mv,g))+'+'+str(sum([(i-sum(mv)/len(mv))*(j-sum(g)/len(g)) for i,j in zip(mv,g)])/sum([(i-sum(g)/len(g))**2 for i in g]))+'*g')([int(i) for i in input("Please input mv:").split()],[int(i) for i in input("Please input g:").split()]))
