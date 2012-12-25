1a4fb50475c9844770ee28515934932694fdd7b0
枫叶饭团 <maplebeats@gmail.com>
2012-12-25 18:20:29 +0800

    update

--- stardict.sh
+++ stardict.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 #绑定脚本到到meta+d
 
+speakdir="/home/maplebeats/Documents/dir"
 words=$(xsel -p)
 icon='/usr/share/icons/gnome/24x24/apps/zen-icon.png' 
 for w in $words
-do
+do  
+    declare -l w #lower
     tran=`sdcv -n --utf8-output "$w"`
     notify-send -a 'dict' -i $icon "$w" "${tran:0:300}"
+    mplayer "$speakdir"/z_"$w"__gb_1.wav
 done
