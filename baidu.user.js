97dddf1221c9c325c0c30d4e52931fae960b4b28
枫叶饭团 <maplebeats@gmail.com>
2013-01-24 13:31:00 +0800

    update

--- baidu.user.js
+++ baidu.user.js
@@ -12,37 +12,37 @@ var JQueryDiv = document.createElement("div");
 JQueryDiv.setAttribute("onclick", "return $;");
 $ = JQueryDiv.onclick();
 
-var bar_name=unsafeWindow.PageData.forum.name; //BUG 签到C++贴吧的时候post数据为C
+var bar_name=unsafeWindow.PageData.forum.name; 
 var tbs=unsafeWindow.PageData.tbs;
 var is_like=unsafeWindow.PageData.user.is_like;
 var is_sign=unsafeWindow.PageData.is_sign_in;
+var data = {ie:"=utf-8", kw:bar_name, tbs:tbs };
 
 function sign(){
 	if(is_like&&!is_sign){
-		//发送
-		$.post("http://tieba.baidu.com/sign/add","ie=utf-8&kw="+bar_name+"&tbs="+tbs,function callback(Ajax_data){
+		$.post("http://tieba.baidu.com/sign/add", data, function callback(Ajax_data){
 			if(!Ajax_data.error){
-				//成功
 				is_sign=1;
                 location.reload();
 			}
 		},"json");
-        return true;
 	}
     return false;
 }
+var scount = 1;
 function srequest(){
+    console.log(scount++);
     if(!sign()){
         setTimeout(srequest, 100);
     }
 }
 var d = new Date();
-var t = (60-d.getMinutes())*60*1000 + (1000-d.getMilliseconds());
-if(t>0 && d.getHours>0){
+var t = ((23-d.getHours())*60 + (60-d.getMinutes()))*60*1000 + (1000-d.getMilliseconds());
+console.log(t);
+if(d.getHours() != 0){
     var s = setTimeout(srequest, t);
+    is_sign = 0; //假定没签到。。
 }else{
     srequest();
 }
-
-//事件型
 window.addEventListener('load', sign, false);
