d9e46d21a722e9c9cab5f853e98299c7ea1990ad
(≧▽≦)y <maplebeats@gmail.com>
2015-06-08 20:27:11 +0800

    add wx.js

---
+++ wx.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// // @name        wx
+// // @namespace   wx
+// // @description wx自动跳转
+// // @include     http://*support.weixin.qq.com/cgi-bin/*
+// // @version     0.0.1
+// // @Author: maplebeats
+// // @mail: maplebeats@gmail.com
+// // @run-at document-end
+// // ==/UserScript==
+
+var url_div = document.getElementsByClassName("url");
+var url = url_div[0].innerHTML;
+console.log(url);
+document.getElementsByClassName("url")[0].innerHTML='<a id="target" href="'+url+'">'+url+'</a>';
+var a = document.getElementById("target");
+var evt = document.createEvent("MouseEvents");
+evt.initEvent("click", true, true); 
+a.dispatchEvent(evt); 
