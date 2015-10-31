2050e38bf38f70a9f28211ea863b97039c48f609
(≧▽≦)y <maplebeats@gmail.com>
2015-11-01 01:00:29 +0800

    fix escape bug

--- wx.js
+++ wx.js
@@ -4,14 +4,14 @@
 // @description wx自动跳转
 // @include     http://*support.weixin.qq.com/cgi-bin/*
 // @downloadURL https://raw.githubusercontent.com/maplebeats/Scripts/master/wx.js
-// @version     0.0.2
+// @version     0.0.3
 // @Author: maplebeats
 // @mail: maplebeats@gmail.com
 // @run-at document-end
 // ==/UserScript==
 
 var url_div = document.getElementsByClassName("url");
-var url = url_div[0].innerHTML;
+var url = unescape(url_div[0].innerHTML);
 console.log(url);
 document.getElementsByClassName("url")[0].innerHTML='<a id="target" href="'+url+'">自动跳转</a>';
 var a = document.getElementById("target");
