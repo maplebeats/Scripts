bd5ff37aa68f7368651297cd57cbc1d580eba22c
(≧▽≦)y <maplebeats@gmail.com>
2015-11-01 01:02:46 +0800

    fix escape bug v2

--- wx.js
+++ wx.js
@@ -4,7 +4,7 @@
 // @description wx自动跳转
 // @include     http://*support.weixin.qq.com/cgi-bin/*
 // @downloadURL https://raw.githubusercontent.com/maplebeats/Scripts/master/wx.js
-// @version     0.0.3
+// @version     0.0.4
 // @Author: maplebeats
 // @mail: maplebeats@gmail.com
 // @run-at document-end
@@ -13,7 +13,7 @@
 var url_div = document.getElementsByClassName("url");
 var url = unescape(url_div[0].innerHTML);
 console.log(url);
-document.getElementsByClassName("url")[0].innerHTML='<a id="target" href="'+url+'">自动跳转</a>';
+document.getElementsByClassName("url")[0].innerHTML='<a id="target" href="'+unescape(url)+'">自动跳转</a>';
 var a = document.getElementById("target");
 var evt = document.createEvent("MouseEvents");
 evt.initEvent("click", true, true);
